c8a88f25fc73dc095591b693e9fccde49a5bf519
AKB428 <gandara016+1@gmail.com>
2022-07-29 01:06:20 +0900

    DDLを絵文字に対応

--- DDL/twitter_trends.sql
+++ DDL/twitter_trends.sql
@@ -1,11 +1,11 @@
 CREATE TABLE `twitter_trends` (
   `id` int NOT NULL AUTO_INCREMENT,
   `rank` int DEFAULT NULL,
-  `name` varchar(100) CHARACTER SET utf8 COLLATE utf8_unicode_ci DEFAULT NULL,
+  `name` varchar(100) DEFAULT NULL,
   `created_at` datetime DEFAULT NULL,
   `updated_at` datetime DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `index_twitter_trends_on_name` (`name`),
   KEY `index_twitter_trends_on_created_at` (`created_at`),
   KEY `index_twitter_trend_on_rank` (`rank`)
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb3 COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
